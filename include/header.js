86aa8b38c4ffc1f6b57167e807606eb873c8b45b
jdwcg <bigcloudjo@gmail.com>
2023-03-30 11:23:25 +0900

    v

--- include/header.js
+++ include/header.js
@@ -6,24 +6,25 @@ $(function () {
    ]
 
    for (let i = 0; i < gnb.length; i++) {
-      $(".gnbWrap").append(`<li><a href='${gnb[i].url}'>${gnb[i].tit}</a></li>`)
+      $(".gnb_wrap").append(`<li><a href='${gnb[i].url}'>${gnb[i].tit}</a></li>`)
    }
+
    let lnb1 = [
-      { tit: "lnb_d", url: "./pages/test.html" },
-      { tit: "lnb_e", url: "./pages/test.html" },
-      { tit: "lnb_f", url: "./pages/test.html" },
-      { tit: "lnb_g", url: "./pages/test.html" },
+      { tit: "lnb_d", url: "./pages/test/html" },
+      { tit: "lnb_e", url: "./pages/test/html" },
+      { tit: "lnb_f", url: "./pages/test/html" },
+      { tit: "lnb_g", url: "./pages/test/html" },
    ]
    let lnb2 = [
-      { tit: "lnb_h", url: "./pages/test.html" },
-      { tit: "lnb_i", url: "./pages/test.html" },
-      { tit: "lnb_j", url: "./pages/test.html" },
-      { tit: "lnb_k", url: "./pages/test.html" },
+      { tit: "lnb_h", url: "./pages/test/html" },
+      { tit: "lnb_i", url: "./pages/test/html" },
+      { tit: "lnb_j", url: "./pages/test/html" },
+      { tit: "lnb_k", url: "./pages/test/html" },
    ]
    for (let i = 0; i < lnb1.length; i++) {
-      $(".gnbWrap > li:nth-child(1)").append(`<ul><li><a href='${lnb1[i].url}'>${lnb1[i].tit}</a></li></ul>`)
+      $(".gnb_wrap > li:nth-child(1)").append(`<ul><li><a href='${lnb1[i].url}'>${lnb1[i].tit}</a></li></ul>`)
    }
    for (let i = 0; i < lnb2.length; i++) {
-      $(".gnbWrap > li:nth-child(2)").append(`<ul><li><a href='${lnb2[i].url}'>${lnb2[i].tit}</a></li></ul>`)
+      $(".gnb_wrap > li:nth-child(2)").append(`<ul><li><a href='${lnb2[i].url}'>${lnb2[i].tit}</a></li></ul>`)
    }
 })
